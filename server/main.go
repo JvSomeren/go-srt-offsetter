2c4445b8891af05755425310cc6bd54d4b3f71a4
Joost van Someren <joost.v.someren@gmail.com>
2020-02-28 13:29:49 +0100

    add GetEnv helper function for environment variables

--- server/main.go
+++ server/main.go
@@ -1,6 +1,18 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+)
+
+// GetEnv returns a set environment variable based on a passed key or
+// if the variable isn't set the passed fallback
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
 
 func main() {
 	srv := CreateServer()
