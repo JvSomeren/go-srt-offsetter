57cf02c610bb3b661a2c0b4d2ac5e93c3faba15a
Joost van Someren <joost.v.someren@gmail.com>
2020-02-28 21:11:30 +0100

    add backend call to slideUp

--- webapp/src/Components/SlideUp.svelte
+++ webapp/src/Components/SlideUp.svelte
@@ -7,6 +7,37 @@
 	
 	let offsetContainer = {};
 
+	const closeSlideUp = () => {
+		slideUp.close();
+	};
+
+	const updateOffset = debounce(function(path) {
+		if(offsetContainer[path].deltaOffset !== 0) {
+			const data = {
+				subtitle: path,
+				offset: offsetContainer[path].deltaOffset,
+			};
+			const res = fetch(`${process.env.apiUrl}/subtitle`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			})
+				.then(res => res.json())
+				.catch(error => console.error(error));
+
+			offsetContainer[path].deltaOffset = 0;
+		}
+	}, 500);
+
+	const onClick = (offset) => {
+		offsetContainer[$slideUp.path].deltaOffset += offset;
+		offsetContainer[$slideUp.path].currentOffset += offset;
+
+		updateOffset($slideUp.path);
+	};
+
 	const unsubscribeSlideUp = slideUp.subscribe((v) => {
 		if(!offsetContainer.hasOwnProperty($slideUp.path)) {
 			offsetContainer[$slideUp.path] = {
@@ -15,25 +46,6 @@
 			};
 		}
 	});
-
-	const closeSlideUp = () => {
-		slideUp.close();
-	};
-
-	const updateOffset = debounce(function(path) {
-		if(offsetContainer[path].deltaOffset !== 0) {
-			// use deltaOffset here to sent request
-			console.log('Offset updated with:', offsetContainer[path].deltaOffset, path);
-			offsetContainer[path].deltaOffset = 0;
-		}
-	}, 1000);
-
-	const onClick = (offset) => {
-		offsetContainer[$slideUp.path].deltaOffset += offset;
-		offsetContainer[$slideUp.path].currentOffset += offset;
-
-		updateOffset($slideUp.path);
-	};
 
 	onDestroy(async () => {
 		unsubscribeSlideUp();
@@ -55,13 +67,15 @@
 		</h2>
 		<h3 class="text-sm rounded-full bg-gray-200 text-gray-700 font-semibold mx-auto inline-block px-3 py-1 lowercase">{languages[$slideUp.subtitle.language]}</h3>
 
-		<p>{offsetContainer[$slideUp.path].currentOffset}</p>
+		<div class="px-3 py-4">
+			<p>Current offset: {offsetContainer[$slideUp.path].currentOffset}ms</p>
+		</div>
 
     <div class="flex justify-around mx-auto mt-2 text-xs">
-      <button class="btn btn-red">-100ms</button>
+      <button class="btn btn-red" on:click={() => onClick(-100)}>-100ms</button>
       <button class="btn btn-red" on:click={() => onClick(-50)}>-50ms</button>
       <button class="btn btn-green" on:click={() => onClick(50)}>+50ms</button>
-      <button class="btn btn-green">+100ms</button>
+      <button class="btn btn-green" on:click={() => onClick(100)}>+100ms</button>
     </div>
   </aside>
 {/if}
