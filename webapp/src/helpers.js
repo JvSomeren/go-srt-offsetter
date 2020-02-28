a18885e06365777264dd1263cbbb0f72ead94395
Joost van Someren <joost.v.someren@gmail.com>
2020-02-28 20:48:24 +0100

    add debounce helper

---
+++ webapp/src/helpers.js
@@ -0,0 +1,11 @@
+export const debounce = (fn, delay) => {
+  let timeout;
+
+  return function() {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, delay);
+  }
+};
